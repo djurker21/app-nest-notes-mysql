ae1a3fe0ef87d34081b28a4afbd66167d43cc195
Yurii Vydai <djurker21@gmail.com>
2021-09-07 13:17:38 +0300

    entities fields, many to one

--- src/notes/entities/note.entity.ts
+++ src/notes/entities/note.entity.ts
@@ -1,1 +1,14 @@
-export class Note {}
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
+import { User } from '../../users/entities/user.entity';
+
+@Entity()
+export class Note {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  text: string;
+
+  @ManyToOne(() => User)
+  user: User;
+}
