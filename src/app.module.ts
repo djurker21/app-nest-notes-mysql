2aaefda8e187093a33e3a362d43e5fe499e1bf6d
Yurii Vydai <djurker21@gmail.com>
2021-09-07 13:04:53 +0300

    users, notes crud

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { NotesModule } from './notes/notes.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule, NotesModule],
   controllers: [AppController],
   providers: [AppService],
 })
